f345e22491e1fd28ee2b50c4ac87e66586e2d5ff
ecupaio <ecupaio@gmail.com>
2020-08-06 11:03:29 -0400

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    imagemin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'img/',
+          src: ['**/*.{png,jpg,gif,jpeg,svg}'],
+          dest: 'img/'
+        }]
+      }
+    },
+    watch: {
+      imagemin: {
+        files: 'img/*.{png,jpg,gif}',
+        tasks: ['imagemin']
+      },
+      uglify: {
+        files: ['js/*.js',"js/min/!*.min.js"],
+        tasks: ['newer:uglify:target']
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: "js/",
+          src: ["*.js", "!*.min.js"],
+          dest: "js/min/",
+          ext: ".min.js"
+        }]
+      }
+    },
+    shell: {
+      installBundle: {
+        command: 'bundle install'
+      },
+      jekyllServe: {
+        command: 'bundle exec jekyll serve --livereload '
+      }
+    },
+		concurrent: {
+      //target: ['shell:jekyllServe','watch'],
+      target: ['watch'],
+			options: {
+				logConcurrentOutput: true
+			}
+		}
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.registerTask('default', ['uglify','concurrent:target']);
+};
