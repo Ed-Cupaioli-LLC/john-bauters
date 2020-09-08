6f84c3828a8aed101f07ae9558c3a96f2b77da20
Ed Cupaioli <ecupaio@gmail.com>
2020-09-08 12:36:05 -0400

    added shaddow to nav config updates form

--- js/site_functions.js
+++ js/site_functions.js
@@ -9,23 +9,35 @@ $(function(){
     $('#form-overlay').addClass('active');
   });
   // Contact form 
-  $('#contact-form').submit(function(e) {
+  $('.update-form').submit(function(e) {
     e.preventDefault();
+    var formId = $(this).attr('id');
+    formId = "#"+formId;
     $.ajax({
-      url: 'https://docs.google.com/forms/d/e/1FAIpQLSdBUbyKOEO_Nwr1huDCRB2t6qn0CZenRxIHjFyx61lEw9CaBQ/formResponse',     
-      data: $('#contact-form').serialize(), 
-      type: 'POST', 
-      dataType: "json", 
-       
+      url: 'https://docs.google.com/forms/d/e/1FAIpQLSe-iyg6KnTtvk1Tet43-4woEwZMMV_8gscEglLsLrpMkDVZOg/formResponse',     
+      data: $(formId).serialize(), 
+      type: 'GET',
+      crossorigin: true 
     }).always(function() {
-      $('#contact-form .form-success').addClass('active');
+      $(formId+'.update-form .form-body').slideUp();
+      $(formId+'.update-form .form-success').slideDown();
     });
   });
-
+  //Nav functions
   $('.mobile-nav-toggle').click(function() {
+    console.log('clicked');
     $(this).toggleClass('active');
     $('.nav-links').toggleClass('active');
   });
   
+  $(window).on('scroll',function(e) {    
+    console.log(e);
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $('#site-nav').addClass('sticky');
+    } else {
+      $('#site-nav').removeClass('sticky');
+    }
+  });
 });
 
