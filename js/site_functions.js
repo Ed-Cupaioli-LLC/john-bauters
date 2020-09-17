57026f7977bc8bc5e92a7b0d426e5531f88782ff
Ed Cupaioli <ecupaio@gmail.com>
2020-09-17 18:16:23 -0400

    integrated mail chimps embeddable form

--- js/site_functions.js
+++ js/site_functions.js
@@ -8,28 +8,7 @@ $(function(){
     $('#contact-form').addClass('active');
     $('#form-overlay').addClass('active');
   });
-  // Contact form 
-  // Mailchimp API 9d509d30cf6c52a850b0d37521950352-us2
-  $('.update-form').submit(function(e) {
-    e.preventDefault();
-    var formId = $(this).attr('id');
-    formId = "#"+formId;
-    $.ajax({
-      url: 'https://johnbauters.us2.list-manage.com/subscribe/post?u=d1f6edf445667bd79d1370688&amp;id=8d8f66dcce',     
-      data: $(formId).serialize(), 
-      type: 'POST',
-      crossorigin: true,
-      success: function(data) {
-        console.log(data);
-      },
-      error: function(data) {
-        console.log(data);
-      }
-    }).always(function() {
-      $(formId+'.update-form .form-body').slideUp();
-      $(formId+'.update-form .form-success').slideDown();
-    });
-  });
+  
   //Nav functions
   $('.mobile-nav-toggle').click(function() {
     console.log('clicked');
